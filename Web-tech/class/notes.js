8093c477132a2a0e5309a1258ac76b3cc4faee53
Tejshree Karande <tejshreekarande23@gmail.com>
2026-01-20 09:48:37 +0530

    Notes,variables

---
+++ Web-tech/class/notes.js
@@ -0,0 +1,14 @@
+// *what is web technology?
+// --> web technology means tools and techniques to create, run, and manage websites and web applications on the internet (like HTML, CSS, JavaScript).
+
+// *Real time example of web technology:
+// -->Amazon, Flipkart, Instagram, Facebook,etc.
+
+// *What is JavaScript?
+// -->JavaScript is scripting language whose code runs at runtime inside the browser used in web development to create dynamic, interactive, and responsive web pages.
+
+// *Which file extension is used for JavaScript?
+// --> ".js" extension is used for JavaScript.
+
+// *Why do we use Node.js?
+// -->Node.js is used to run JavaScript outside the browser and to create fast backend applications easily.
